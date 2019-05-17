372bf730cbdc3397e7fc210c96de85e7aeacbf26
Adriaan Knapen <hi@aknapen.nl>
2019-05-17 23:09:06 +0300

    fix: added missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFireStorageModule} from "@angular/fire/storage";
 
 @NgModule({
   imports: [
