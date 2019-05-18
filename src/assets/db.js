308122c6a5ab3f474535a57bcd15e10767323385
Federico Macchi <federico.macchi@mail.polimi.it>
2019-05-18 10:12:04 +0300

    added db

---
+++ src/assets/db.js
@@ -0,0 +1,79 @@
+export const db = {
+  memes: [
+    {
+      url: 'assets/img/memes/meme1.jpg',
+      title: 'Whadya think about my meme?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'assets/img/memes/meme2.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://www.memecreator.org/static/images/memes/3702262.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://www.tampabay.com/storyimage/HI/20150107/ARTICLE/301079512/AR/0/AR-301079512.jpg&MaxW=1200&Q=66',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.redd.it/fakhj7o7eaaz.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.imgflip.com/3179kg.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    ///
+    {
+      url: 'https://i.imgflip.com/312pi1.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.imgflip.com/314hqq.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/480/657/414.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/477/630/00b.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/475/740/bb1.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    {
+      url: 'https://images.complex.com/complex/images/c_fill,g_center,w_1200/fl_lossy,pg_1,q_auto/bujewhyvyyg08gjksyqh/spongebob',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+  ],
+  "0": {
+    userId: 0,
+    userName: "Federico",
+    userMeme: [
+      {
+        id: 1,
+        url: 'assets/img/memes/meme1.jpg',
+        title: 'Whadya think about my meme?',
+        text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+      },
+    ],
+    liked_meme: [],
+    matches: [],
+  },
+}
