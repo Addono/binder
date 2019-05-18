7a1e3baf105580fd1c6a2c5a700b9ef7931a1280
Federico Macchi <federico.macchi@mail.polimi.it>
2019-05-18 12:37:40 +0300

    added dist folder

---
+++ dist/assets/db.js
@@ -0,0 +1,120 @@
+export const rootDb = {
+  memes: {
+    0: {
+      url: 'assets/img/memes/meme1.jpg',
+      title: 'Whadya think about my meme?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    1: {
+      url: 'assets/img/memes/meme2.jpg',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    2: {
+      url: 'https://www.memecreator.org/static/images/memes/3702262.jpg',
+      title: 'Me, at the shared kitchen',
+      text: 'Its an old meme sir, but it still checks out',
+    },
+    3: {
+      url: 'https://www.tampabay.com/storyimage/HI/20150107/ARTICLE/301079512/AR/0/AR-301079512.jpg&MaxW=1200&Q=66',
+      title: 'BLB',
+      text: 'Fix your bad luck, match with me!',
+    },
+    4: {
+      url: 'https://i.redd.it/fakhj7o7eaaz.jpg',
+      title: 'Isnt it cold in here?',
+      text: 'Have you ever heard the tragedy of darth plagueis the wise?',
+    },
+    5: {
+      url: 'https://i.imgflip.com/3179kg.jpg',
+      title: 'RIP',
+      text: 'The only day that you can post this',
+    },
+    ///
+    6: {
+      url: 'https://i.imgflip.com/312pi1.jpg',
+      title: 'You cant just downvote this meme',
+      text: 'And you shouldnt downvote me either',
+    },
+    7: {
+      url: 'https://i.imgflip.com/314hqq.jpg',
+      title: 'The onion that can make you both laugh and cry',
+      text: 'I can also do that to you',
+    },
+    8: {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/480/657/414.jpg',
+      title: 'Looks like you forgot your Finnish lessons again',
+      text: 'Like you forgot to lock your front door',
+    },
+    9: {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/477/630/00b.jpg',
+      title: 'Everything?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    10: {
+      url: 'https://i.kym-cdn.com/photos/images/masonry/001/475/740/bb1.jpg',
+      title: 'No, por favor!!!',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+    11: {
+      url: 'https://images.complex.com/complex/images/c_fill,g_center,w_1200/fl_lossy,pg_1,q_auto/bujewhyvyyg08gjksyqh/spongebob',
+      title: 'I like... Teletubbies... Hen.. HMMM?',
+      text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+    },
+  },
+  "0": {
+    userId: 0,
+    userName: "Federico",
+    gen: 0,
+    userMeme: [
+      {
+        id: 1,
+        url: 'assets/img/memes/meme1.jpg',
+        title: 'Whadya think about my meme?',
+        text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+      },
+    ],
+    liked_meme: [],
+    matches: [],
+  },
+  "1": {
+    userId: 1,
+    userName: "Marios",
+    gen: 0,
+    userMeme: [4],
+    liked_meme: [],
+    matches: [],
+  },
+  "2": {
+    userId: 2,
+    userName: "Adriaan",
+    gen: 0,
+    userMeme: [6],
+    liked_meme: [],
+    matches: [],
+  },
+  "3": {
+    userId: 3,
+    userName: "Elena",
+    gen: 1,
+    userMeme: [3],
+    liked_meme: [],
+    matches: [],
+  },
+  "4": {
+    userId: 4,
+    userName: "Veera",
+    gen: 1,
+    userMeme: [5],
+    liked_meme: [],
+    matches: [],
+  },
+  "5": {
+    userId: 5,
+    userName: "Maria",
+    gen: 1,
+    userMeme: [1],
+    liked_meme: [],
+    matches: [],
+  },
+}
