0eba2fd5ab7e8822eab949ddca3e95b4eb9a76e2
Marios Chatiras <riosma@users.noreply.github.com>
2019-05-18 11:16:11 +0300

    with meme id

--- src/assets/db.js
+++ src/assets/db.js
@@ -1,62 +1,74 @@
 export const db = {
   memes: [
     {
+	  id: 0
       url: 'assets/img/memes/meme1.jpg',
       title: 'Whadya think about my meme?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 1
       url: 'assets/img/memes/meme2.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 2
       url: 'https://www.memecreator.org/static/images/memes/3702262.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 3
       url: 'https://www.tampabay.com/storyimage/HI/20150107/ARTICLE/301079512/AR/0/AR-301079512.jpg&MaxW=1200&Q=66',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 4
       url: 'https://i.redd.it/fakhj7o7eaaz.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 5
       url: 'https://i.imgflip.com/3179kg.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     ///
     {
+	  id: 6
       url: 'https://i.imgflip.com/312pi1.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 7
       url: 'https://i.imgflip.com/314hqq.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 8
       url: 'https://i.kym-cdn.com/photos/images/masonry/001/480/657/414.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 9
       url: 'https://i.kym-cdn.com/photos/images/masonry/001/477/630/00b.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 10
       url: 'https://i.kym-cdn.com/photos/images/masonry/001/475/740/bb1.jpg',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
     },
     {
+	  id: 11
       url: 'https://images.complex.com/complex/images/c_fill,g_center,w_1200/fl_lossy,pg_1,q_auto/bujewhyvyyg08gjksyqh/spongebob',
       title: 'I like... Teletubbies... Hen.. HMMM?',
       text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
@@ -69,6 +81,34 @@ export const db = {
       {
         id: 1,
         url: 'assets/img/memes/meme1.jpg',
+        title: 'Whadya think about my meme?',
+        text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+      },
+    ],
+    liked_meme: [],
+    matches: [],
+  },
+  "1": {
+    userId: 1,
+    userName: "Adriaan",
+    userMeme: [
+      {
+        id: 2,
+        url: 'assets/img/memes/fakhj7o7eaaz.jpg',
+        title: 'Whadya think about my meme?',
+        text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
+      },
+    ],
+    liked_meme: [],
+    matches: [],
+  },
+  "2": {
+    userId: 2,
+    userName: "Marios",
+    userMeme: [
+      {
+        id: 3,
+        url: 'assets/img/memes/312pi1.jpg',
         title: 'Whadya think about my meme?',
         text: 'The user can chose this text, and yes I typed this randomly, cause I don\'t like lorem. IPSUM.',
       },
