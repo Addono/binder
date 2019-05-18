87c62f87dba637a5091ba4bebe57cacac8141431
Adriaan Knapen <hi@aknapen.nl>
2019-05-18 13:55:02 +0300

    build

--- src/app/shared/database.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { rootDb } from '../../assets/db.js';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class DatabaseService {
-  public db = rootDb;
-}
